eaa099529b2e8630525f52587ea0110b9aa2b3c4
Edgar Klenske <edgar.klenske@tuebingen.mpg.de>
2017-03-31 12:08:19 +0200

    added compile-script

---
+++ matlab/compile_utilities.m
@@ -0,0 +1,8 @@
+% compiling the mex files of some utilities
+%
+% (C) 2017 Max-Planck-Institute for Intelligent Systems
+%
+% Author: Edgar D. Klenske, 2017
+
+% compile utilities
+mex -I../cpp/ ../cpp/joint_min.cpp
